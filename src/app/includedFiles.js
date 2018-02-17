0e400280d455152af090a733ae3adce6bf537f59
John Flynn <JJimmyFlynn@gmail.com>
2018-02-17 12:49:49 -0500

    initial sass + wordpress  implementation

---
+++ src/app/includedFiles.js
@@ -0,0 +1,19 @@
+export const coreFiles = [
+	'base/_base.scss',
+	'base/_typography.scss',
+	'components/_buttons.scss',
+	'components/_forms.scss',
+	'modules/_header.scss',
+	'modules/_footer.scss',
+	'pages/_general.scss',
+	'pages/_home.scss',
+	'utilities/_mixins.scss',
+	'utilities/_variables.scss',
+	'vendor/_normalize.scss',
+	'main.scss'
+]
+
+export const wordpressFiles = [
+	'base/_wordpress.scss',
+	'pages/_posts.scss'
+]
--- src/app/index.js
+++ src/app/index.js
@@ -1,10 +1,51 @@
 import Yeoman from 'yeoman-generator';
 import chalk from 'chalk';
+import { coreFiles, wordpressFiles } from './includedFiles';
 
 class FlynnStyles extends Yeoman {
+
   welcome() {
-    this.log('It worked!');
+    this.log(chalk.blue('Welcome to the Flynndustries Styles Generator!'))
   }
+
+  prompts() {
+    return this.prompt([
+      {
+        type: 'input',
+        name: 'basePath',
+        message: 'Where should the files be copied (relative to current directory)?',
+        default: 'sass/'
+      },
+      {
+        type: 'input',
+        name: 'projectName',
+        message: "What is the name of the theme or projec?"
+      },
+      {
+        type: 'input',
+        name: 'author',
+        message: "Who is the author?",
+        default: "John Flynn"
+      }
+    ]).then(props => {
+        this.options.basePath = props.basePath;
+        this.options.projectName = props.projectName;
+        this.options.author = props.author;
+      }
+    );
+  }
+
+  copyCoreFiles() {
+    coreFiles.forEach(file => this.fs.copyTpl(
+      this.templatePath(this.options.basePath + file),
+      this.destinationPath(this.options.basePath + file),
+      {
+        project: this.options.projectName,
+        author: this.options.author
+      }
+    ));
+  }
+  
 };
 
 export default FlynnStyles;
