7912cb1d0b77cdccca7098c8374ae7ed9990dcf5
John Flynn <JJimmyFlynn@gmail.com>
2018-02-25 18:01:05 -0500

    eslint

--- src/app/index.js
+++ src/app/index.js
@@ -1,10 +1,8 @@
 import Yeoman from 'yeoman-generator';
 import chalk from 'chalk';
-import { 
-  coreFiles, wordpressFiles, shopifyFiles } from './includedFiles';
+import { coreFiles, wordpressFiles, shopifyFiles } from './includedFiles';
 
 class FlynnStyles extends Yeoman {
-
   welcome() {
     this.log(chalk.blue('Welcome to the Flynndustries Styles Generator!'))
   }
@@ -17,12 +15,12 @@ class FlynnStyles extends Yeoman {
         message: 'Is this a WordPress or Shopify project?',
         choices: [
           {
-            name: "WordPress",
-            value: "wordpress"
+            name: 'WordPress',
+            value: 'wordpress'
           },
           {
-            name: "Shopify",
-            value: "shopify"
+            name: 'Shopify',
+            value: 'shopify'
           }
         ]
       },
@@ -35,60 +33,63 @@ class FlynnStyles extends Yeoman {
       {
         type: 'input',
         name: 'projectName',
-        message: "What is the name of the theme or projec?"
+        message: 'What is the name of the theme or projec?'
       },
       {
         type: 'input',
         name: 'author',
-        message: "Who is the author?",
-        default: "John Flynn"
+        message: 'Who is the author?',
+        default: 'John Flynn'
       }
     ]).then(props => {
-        this.options.projectType = props.projectType;
-        this.options.basePath = props.basePath;
-        this.options.projectName = props.projectName;
-        this.options.author = props.author;
-      }
-    );
+      this.options.projectType = props.projectType;
+      this.options.basePath = props.basePath;
+      this.options.projectName = props.projectName;
+      this.options.author = props.author;
+    });
   }
 
   copyCoreFiles() {
     coreFiles.forEach(file => this.fs.copyTpl(
-      this.templatePath(this.options.basePath + file),
-      this.destinationPath(this.options.basePath + file),
-      {
-        project: this.options.projectName,
-        author: this.options.author
-      }
-    ));
-  }
-
-  copyShopifyFiles() {
-    if (this.options.projectType == 'shopify') {
-      shopifyFiles.forEach(file => this.fs.copyTpl(
         this.templatePath(this.options.basePath + file),
         this.destinationPath(this.options.basePath + file),
         {
           project: this.options.projectName,
           author: this.options.author
         }
-      ));
+      )
+    );
+  }
+
+  copyShopifyFiles() {
+    if (this.options.projectType === 'shopify') {
+      shopifyFiles.forEach(file =>
+        this.fs.copyTpl(
+          this.templatePath(this.options.basePath + file),
+          this.destinationPath(this.options.basePath + file),
+          {
+            project: this.options.projectName,
+            author: this.options.author
+          }
+        )
+      );
     }
   }
 
   copyWordPressFiles() {
-    if (this.options.projectType == 'wordpress') {
-      wordpressFiles.forEach(file => this.fs.copyTpl(
-        this.templatePath(this.options.basePath + file),
-        this.destinationPath(this.options.basePath + file),
-        {
-          project: this.options.projectName,
-          author: this.options.author
-        }
-      ));
+    if (this.options.projectType === 'wordpress') {
+      wordpressFiles.forEach(file =>
+        this.fs.copyTpl(
+          this.templatePath(this.options.basePath + file),
+          this.destinationPath(this.options.basePath + file),
+          {
+            project: this.options.projectName,
+            author: this.options.author
+          }
+        )
+      );
     }
   }
-  
-};
+}
 
 export default FlynnStyles;
