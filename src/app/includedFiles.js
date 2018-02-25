3f703f2c77187d9725add6c82c6948b8468024f3
John Flynn <JJimmyFlynn@gmail.com>
2018-02-25 18:05:45 -0500

    eslint cleanup

--- src/app/includedFiles.js
+++ src/app/includedFiles.js
@@ -1,23 +1,18 @@
 export const coreFiles = [
-	'base/_base.scss',
-	'base/_typography.scss',
-	'components/_buttons.scss',
-	'components/_forms.scss',
-	'modules/_header.scss',
-	'modules/_footer.scss',
-	'pages/_general.scss',
-	'pages/_home.scss',
-	'utilities/_mixins.scss',
-	'utilities/_variables.scss',
-	'vendor/_normalize.scss',
-	'main.scss'
-]
+  'base/_base.scss',
+  'base/_typography.scss',
+  'components/_buttons.scss',
+  'components/_forms.scss',
+  'modules/_header.scss',
+  'modules/_footer.scss',
+  'pages/_general.scss',
+  'pages/_home.scss',
+  'utilities/_mixins.scss',
+  'utilities/_variables.scss',
+  'vendor/_normalize.scss',
+  'main.scss'
+];
 
-export const wordpressFiles = [
-	'base/_wordpress.scss',
-	'pages/_posts.scss'
-]
+export const wordpressFiles = ['base/_wordpress.scss', 'pages/_posts.scss'];
 
-export const shopifyFiles = [
-	'base/_shopify.scss'
-]
+export const shopifyFiles = ['base/_shopify.scss'];
